c44fb4ca7c90bef6b3ad188a608c0fd037a3dbdb
Nicolas <v.nicolasvanhuysse@free.fr>
2021-02-15 11:33:57 +0100

    cv Nicolas Vanhuysse

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nicolas VANHUYSSE - Développeur web</title>
+
+  <!-- CSS -->
+
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/fancybox.css">
+  <link rel="stylesheet" href="CSS/media.css">
+
+  <!-- FONT -->
+
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i" rel="stylesheet">
+
+  <!-- NORMALIZE -->
+
+  <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+
+  <!-- BOOTSTRAP -->
+
+  <link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" />
+
+  <!-- FONTAWESOME -->
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
+
+  <!-- ANIMATE CSS -->
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+  <!-- AOS -->
+
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+  <!-- Script -->
+
+  <script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script data-require="bootstrap@*" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+  <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+  <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+</head>
+
+<body>
+
+  <header>
+    <div class="cover">
+      <div class="home">
+        <hr class="hr1" />
+        <hr class="hr2" />
+        <h1 class="webhead" data-aos="zoom-in">Développeur Web Junior</h1>
+        <h2 class="nomhead" data-aos="zoom-in">Nicolas Vanhuysse</h2>
+      </div>
+    </div>
+
+    <div class="scroll">
+      <a href="#apropos">
+        <div class="box">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </a>
+    </div>
+  </header>
+
+  <div class="blocapropos2" id="apropos">
+    <div class="apropos-left">
+      <h2 data-aos="fade-right">À propos</h2>
+    </div>
+    <div class="apropos-right">
+      <p class="name">Nicolas Vanhuysse | 23 ans</p>
+      <p class="apropos">Après avoir effectué mes études dans le domaine de la Santé, ne me plaisant pas, j’ai décidé de changer de voie.</p>
+      <p class="apropos2"><span class="bold">L’informatique étant une grande passion pour moi</span> depuis mon plus jeune âge, j’ai donc décidé d’intégrer la formation de <span class="bold">Développeur d’Applications Web & Mobile</span> au sein de
+        <span class="bold">POP SCHOOL</span> à Valenciennes.</p>
+      <p class="apropos2">Durant ma formation, j’ai étudié différents langages de programmation tels que <span class="bold">HTML/CSS, PHP, Javascript, SQL</span> ainsi que des frameworks comme <span class="bold">Bootstrap,
+          Laravel, Symfony, Ionic ou Ruby on Rails</span></p>
+      <p class="apropos2"><em>Actuellement, je suis à la recherche d’une alternance pour développer mes connaissances. Si vous êtes intéressé par ma candidature, n’hésitez pas à me <a href="#contact">contacter.</a></em></p>
+    </div>
+  </div>
+
+  <div class="parcours">
+    <h2 id="parcours">Parcours</h2>
+    <h3>Expériences professionnelles</h3>
+    <hr class="hr3" />
+  </div>
+  <div>
+    <p class="date1">Septembre 2019-Février 2020</p>
+    <div id="bloc" class="bloc1" data-aos="zoom-in">
+      <p id="job" class="job1">Stagiaire Développeur Web</p>
+      <p class="ville1">C.G.I.S, Croix</p>
+    </div>
+  </div>
+  <div>
+    <p class="date">Avril 2017-Juillet 2018</p>
+    <div id="bloc" class="bloc2" data-aos="zoom-in">
+      <p id="job" class="job2">Opérateur</p>
+      <p class="ville2">Idéal Fibres & Fabrics, Comines (Belgique)</p>
+    </div>
+  </div>
+  <div>
+    <p class="date">Avril 2016-Avril 2017</p>
+    <div id="bloc" class="bloc3" data-aos="zoom-in">
+      <p id="job" class="job3">Opérateur de production</p>
+      <p class="ville3">Les délices de Comines (Belgique)</p>
+    </div>
+  </div>
+  <div>
+    <p class="date">Février 2016-Mars 2016</p>
+    <div id="bloc" class="bloc4" data-aos="zoom-in">
+      <p id="job" class="job4">Étudiant infirmier</p>
+      <p class="ville4">SSIAD, Somain</p>
+    </div>
+  </div>
+  <div>
+    <p class="date1">Octobre 2015-Novembre 2015</p>
+    <div id="bloc" class="bloc5" data-aos="zoom-in">
+      <p id="job" class="job5">Étudiant infirmier</p>
+      <p class="ville5">EHPAD résidence des Onze Villes, Rieulay</p>
+    </div>
+  </div>
+  <div class="formations">
+    <h3>Formations</h3>
+    <hr class="hr4" />
+  </div>
+  <div>
+    <p class="date3">2019</p>
+    <div id="bloc" class="bloc6" data-aos="zoom-in">
+      <p id="job" class="job6">Développeur Web</p>
+      <p class="ville6">Pop School, Valenciennes</p>
+    </div>
+  </div>
+  <div>
+    <p class="date3">2015-2016</p>
+    <div id="bloc" class="bloc7" data-aos="zoom-in">
+      <p id="job" class="job7">Institut de formation aux métiers de la santé (Infirmier)</p>
+      <p class="ville7">IFMS Centre hospitalier de Valenciennes</p>
+    </div>
+  </div>
+  <div>
+    <p class="date3">2013-2015</p>
+    <div id="bloc" class="bloc8" data-aos="zoom-in">
+      <p id="job" class="job8">Baccalauréat Sciences et technologies de la santé et du social</p>
+      <p class="ville8">Lycée Hélène Boucher, Somain</p>
+    </div>
+  </div>
+  <div>
+    <p class="date3">2012-2013</p>
+    <div id="bloc" class="bloc9" data-aos="zoom-in">
+      <p id="job" class="job9">Seconde générale et technologique</p>
+      <p class="ville9">Lycée Louis Pasteur, Somain</p>
+    </div>
+  </div>
+
+  </div>
+  <div class="competences" id="competences">
+    <h3 class="h3competences">Mes compétences</h3>
+    <div class="bloccompetences" data-aos="zoom-in">
+      <i class="fab fa-html5"></i>
+      <i class="fab fa-css3-alt"></i>
+      <i class="fab fa-php"></i>
+      <i class="fab fa-js-square"></i>
+      <i class="fab fa-git"></i>
+      <i class="fab fa-laravel"></i>
+      <i class="fab fa-symfony"></i>
+    </div>
+    <div class="display">
+      <button class="toggle animated infinite pulse slow delay-1s" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Voir mes réalisations
+      </button>
+    </div>
+  </div>
+  <div class="collapse" id="collapseExample">
+    <div class="card card-body">
+      <div class="fancybox">
+        <a href="./IMG/Serv'Drone.jpg" data-fancybox="images" data-caption="Projet de groupe | Site de vente de drones | Html/CSS"><img src="./IMG/Serv'Drone.jpg" alt="Drone" /></a>
+        <a class="fancy-gamezone" href="./IMG/gamezone.jpg" data-fancybox="images" data-caption="Projet individuel | Site d'un parc d'attraction | Html/CSS/Javascript/Laravel"><img class="fancy-gamezone" src="./IMG/gamezone.jpg" alt="Attraction" /></a>
+        <a class="fancy-gamezone" href="./IMG/Hainaut.png" data-fancybox="images" data-caption="Projet de groupe | Site Europe en Hainaut | Html/CSS/Ruby on Rails"><img class="fancy-gamezone" src="./IMG/Hainaut.png" alt="Hainaut" /></a>
+        <a href="./IMG/violence.png" data-fancybox="images" data-caption="Projet de groupe | Site Violences conjugales | Html/CSS/Ruby on Rails"><img class="fancy-gamezone" src="./IMG/violence.png" alt="Violences" /></a>
+        <a href="./IMG/wordpress.png" data-fancybox="images" data-caption="Projet individuel | Site coach sportif | WordPress"><img class="fancy-gamezone" src="./IMG/wordpress.jpg" alt="Wordpress" /></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="contact" id="contact">
+    <h2>Contact</h2>
+    <div class="bloccontact">
+      <iframe class="googlemaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d68392.2083256615!2d3.1649323450592157!3d50.429166578357695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2c3918fe6616b%3A0x2baf378a2450eba9!2s59870+Marchiennes!5e0!3m2!1sfr!2sfr!4v1553169058418"
+        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div>
+
+      <!-- Modal -->
+
+      <button type="button" class="buttonmail" data-toggle="modal" data-target="#myModal">Me contacter</button>
+
+      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Me contacter</h4>
+            </div>
+            <div class="modal-body">
+              <form action="#competences" method="post">
+                <div class="form-group">
+                  <label for="recipient-name sujet" class="control-label">Nom:</label>
+                  <input type="text" class="form-control" name="sujet" id="recipient-name sujet">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name email" class="control-label">Email:</label>
+                  <input type="text" class="form-control" name="email" id="recipient-name email">
+                </div>
+                <div class="form-group">
+                  <label for="message-text message" class="control-label">Message:</label>
+                  <textarea class="form-control" name="message" id="message-text message"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+              <input type="submit" value="Envoyer" class="btn btn-primary" />
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
+
+      <!-- Script PHP -->
+
+      <?php
+      if(isset($_POST['email']) and isset($_POST['sujet']) and isset($_POST['message']))
+      {
+      $destinataire = 'v.nicolasvanhuysse@free.fr';
+      $email = htmlentities($_POST['email']);
+      if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',str_replace('&amp;','&',$email)))
+      {
+        $sujet = 'Contact: '.stripslashes($_POST['sujet']);
+        $message = stripslashes($_POST['message']);
+        $headers = "From: <".$email.">\n";
+        $headers .= "Reply-To: ".$email."\n";
+       $headers .= "Content-Type: text/plain; charset=\"iso-8859-1\"";
+        if(mail($destinataire,$sujet,$message,$headers))
+       {
+       echo "<p class='message'>Votre message a bien &eacute;t&eacute; envoy&eacute;.</p>";
+        }
+        else
+        {
+        echo "<p class='message'>Une erreur c'est produite lors de l'envois du message.</p>";
+        }
+        }
+        else
+        {
+        echo "<p class='message'>L'email que vous avez entr&eacute; est invalide.</p>";
+        }
+      }
+      else
+      {}
+      ?>
+      <button class="buttoncv" onclick="window.open('Cv_Vanhuysse_Nicolas_2019.pdf')"><i></i> Télécharger mon CV</button>
+      <div class="blocreseau">
+        <div class="frama framag">
+          <a href="https://framagit.org/nicolasvanhuysse" target="_blank"><i class="fab fa-gitlab reseau"></i></a>
+        </div>
+        <div class="frama">
+          <a href="https://www.linkedin.com/in/nicolas-vanhuysse-a0a029189/" target="_blank"><i class="fab fa-linkedin-in reseau2"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="pcopyright">
+        <p>© Copyright 2019 VANHUYSSE NICOLAS. Tous droits réservés</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Script js -->
+
+  <script> AOS.init(); </script>
+
+</body>
+
+</html>
